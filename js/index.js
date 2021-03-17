bb70f39feac80a2b3a9ed3d1c704cc38a1f278b7
Richino <archerrichino@gmail.com>
2021-03-17 01:08:35 -0400

    finish

--- js/index.js
+++ js/index.js
@@ -1,1 +1,47 @@
-// Your code goes here
+//load
+window.addEventListener("load", function () {
+	//declare variables
+	var nav = document.getElementsByClassName("nav-link");
+	//mouseover
+	Array.prototype.forEach.call(nav, function (element) {
+		element.addEventListener("mouseover", function () {
+			element.style.color = "#00adef";
+		});
+
+		element.addEventListener("mouseout", function () {
+			element.style.color = null;
+		});
+	});
+	//keydown
+	window.addEventListener("keydown", function (event) {
+		let easterEggTxt = document.getElementsByClassName("logo-heading");
+		if (event.keyCode === 65) {
+			Array.prototype.forEach.call(easterEggTxt, function (e) {
+				e.innerHTML = "Easter egg has been found";
+				e.select();
+			});
+		}
+	});
+
+	var busImg = document.getElementById("bus");
+	busImg.addEventListener("wheel", function () {
+		busImg.style.width = "80%";
+		busImg.style.margin = "auto";
+	});
+
+	window.addEventListener("scroll", function () {
+		document.getElementsByTagName("body")[0].style.backgroundColor =
+			"#00adef";
+	});
+
+	var content = document.getElementsByClassName("intro");
+	content[0].addEventListener("dblclick", function () {
+		let paragraph = document.getElementById("content-paragraph");
+		paragraph.innerHTML = "Hello Friend";
+	});
+
+	window.addEventListener("resize", function () {
+		document.getElementsByTagName("footer")[0].innerHTML =
+			"Hello Im under the water pls help";
+	});
+});
